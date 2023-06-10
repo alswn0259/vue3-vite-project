749249ad040ff44629445626640e99f52c5e6aa8
kimminjoo <kmjoo0259@naver.com>
2023-06-10 10:42:33 +0900

    add ag-grid

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// ag-grid css
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+
+
 createApp(App).mount('#app')
